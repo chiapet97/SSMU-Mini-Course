2d36a9d3091ea1b06355c55017c2a945b5d35894
chiapet97 <chia.lauren@gmail.com>
2017-05-01 15:18:15 -0400

    Add files via upload
    Hello World Program

---
+++ Class 1/src/Class1.java
@@ -0,0 +1,18 @@
+
+
+/**
+ *
+ * @author Lauren
+ */
+public class Class1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    System.out.println("Hello,world");
+    
+    } 
+    
+}
